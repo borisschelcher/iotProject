5bd9ce0fa9932b1116a28532129e251111d9e92d
bschelcher <borisschelcher@gmail.com>
2022-06-11 18:05:24 +0200

    remove main and add son

--- connection.py
+++ connection.py
@@ -1,4 +1,4 @@
-import variable, game
+import variable, game, music
 import random
 import network
 import ujson
@@ -14,7 +14,7 @@ station.connect(variable.WIFI_SSID, variable.WIFI_PSSWD)
 while not station.isconnected():
     sleep(1)
     print("test")
-
+music.play('Start')
 
 def publish(topic, message):
     jsMsg = ujson.dumps(message)
@@ -23,11 +23,14 @@ def publish(topic, message):
 def sub_callback(topic, msg):
     print((topic, msg))
     print(msg)
-    print(NumberToGuess==int(msg))
-    if msg=='' or msg==None :
+    if NumberToGuess==int(msg):
+        music.play('Win')
         return 0
     else:
-        game.game(int(msg),NumberToGuess)
+        if msg=='' or msg==None :
+            print('error null')
+        else:
+            game.game(int(msg),NumberToGuess)
 
 global client
 client = MQTTClient(client_id = 'ESP-32' , server = '37.187.38.244', port = 1883, user = 'cnam', password = 'cnam2022')
--- game.py
+++ game.py
@@ -3,16 +3,19 @@ import music, time, led
 def game(answer , NumberToGuess ):
     while(answer != NumberToGuess and answer != 0):
         if answer > NumberToGuess :
-            led.blink_r()
+            led.blink_b()
             print("Number is lower : ")
+            music.play('Loose')
             return 0
         elif answer < NumberToGuess:
             led.blink_r()
             print("Number is higher : ")
+            music.play('Loose')
             return 0
         elif answer == NumberToGuess:
             print("You guess it")
-            led.blink_multicolor()            
+            led.blink_multicolor()
+            return 1
         else:
             print("not a good value")
             return 0
